5ece19a62f5135feee9240ae2456879031dea092
AlfalahM273 <MuhammadAlfalah.Madukubah@mitrais.com>
2021-08-31 22:31:30 +0800

    add heroku procfile

--- services/cache.js
+++ services/cache.js
@@ -4,8 +4,11 @@ const redis = require('redis')
 // const util = require('util')
 // client.hget = util.promisify(client.hget);  // enable redis to support promises
 
-const redisUrl = 'redis://127.0.0.1:6379';
-client = redis.createClient(redisUrl);
+client = redis.createClient({
+    host: process.env.REDIS_URL,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+});
 module.exports = {
     client    
 }
