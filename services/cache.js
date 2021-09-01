95815c365891ef7e3dbfca5b23ac42a4b18ca209
AlfalahM273 <MuhammadAlfalah.Madukubah@mitrais.com>
2021-09-01 12:30:16 +0800

    refine redis client

--- services/cache.js
+++ services/cache.js
@@ -1,14 +1,8 @@
-// // insert redis strategy in mongoose exec function
-// const mongoose = require('mongoose')
 const redis = require('redis')
-// const util = require('util')
-// client.hget = util.promisify(client.hget);  // enable redis to support promises
 
 client = redis.createClient({
     host: process.env.REDIS_URL,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD
 });
-module.exports = {
-    client    
-}
+module.exports = client
